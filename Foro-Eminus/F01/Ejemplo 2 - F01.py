8f908703ffe0ee8ecac7e83f7773d452fac2ac99
Xyon0 <final_fantasi_xv@outlook.com>
2025-03-03 21:57:33 -0600

    Update Ejemplo 2 - F01.py

--- Foro-Eminus/F01/Ejemplo 2 - F01.py
+++ Foro-Eminus/F01/Ejemplo 2 - F01.py
@@ -1,12 +1,14 @@
 #team losnadios
 # Este código imprime el nombre de 3 frutas (manzana, platano y naranja) agregando tambien la pera.
 # 2025 / 03/ 03 - V. 1. 0. 0 - SE AGREGAN COMENTARIOS Y ECANBEZADO AL CÓDIGO
+# 2025 /
+# 2025 / 03/ 03 - V. 1. 0. 3 - SE AGREGAN COMENTARIO EN LA LINEA 9, EXPLICANDO EL FUNCIONAMIENTO DEL .append
 # TRABAJARON: Daniel Alberto y Josué
 
 frutas = ["manzana", "plátano", "naranja"] #Se crea una variable llamada "frutas" en la cual contiene elementos que referencian 3 frutas que se utilizaran en el codigo.
 
 
-frutas.append("pera") #Se crea la variable "frutas" y se agrega la funcion .append con la palabra pera, para agregar dicha palabra al final de la lista de frutas.
+frutas.append("pera") #Se usa la variable llamada "frutas", junto con el metodo .append con la palabra pera, siendo esta utilizado para agregar dicha palabra al final de la lista de frutas.
 
 
 print("Lista de frutas:", frutas) #Se llama a imprimir una palanbra siendo "Lista de frutas" para despues usar las variables de "frutas" y finalizando con el elemento del metodo .append.
