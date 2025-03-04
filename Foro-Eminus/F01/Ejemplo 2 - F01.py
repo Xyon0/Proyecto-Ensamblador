598e8ce9b69b32b3abb1ddadbc9cdc7bdd387fd5
McJosue1 <zS23013995@estudiantes.uv.mx>
2025-03-03 21:20:43 -0600

    Update Ejemplo 2 - F01.py

--- Foro-Eminus/F01/Ejemplo 2 - F01.py
+++ Foro-Eminus/F01/Ejemplo 2 - F01.py
@@ -1,9 +1,7 @@
 #TEAM LOSNADIOS
 # Este código imprime el nombre de 3 frutas (manzana, platano y naranja) agregando tambien la pera.
 # 2025 / 03/ 03 - V. 1. 0. 0 - SE AGREGAN COMENTARIOS Y ECANBEZADO AL CÓDIGO
-
-# TRABAJARON: Alejandra y Luis
-
+# TRABAJARON: Daniel Alberto y Josué
 
 frutas = ["manzana", "plátano", "naranja"] #Se trata de imprimir las palabras, manzana, platano y naranja
 
