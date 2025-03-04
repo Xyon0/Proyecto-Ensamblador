ad6429e338f29414e35ff75325fc034e1ed1fd68
Nadiadokky <zs23013987@estudiantes.uv.mx>
2025-03-03 21:45:00 -0600

    Update Ejemplo 3 - F01.py
    en este codigo se explica como se puede determinar si un numero es entero o impar fue realizado con lenguaje phyton y en el trabajaron Nadia y Eduardo

--- Foro-Eminus/F01/Ejemplo 3 - F01.py
+++ Foro-Eminus/F01/Ejemplo 3 - F01.py
@@ -1,6 +1,16 @@
-numero = int(input("Ingrese un número: "))
 
-if numero % 2 == 0:
-    print("El número es par")
+# TEAM Los Nadios
+# ESTE CÓDIGO NOS DICE SI EL NUMERO QUE UN USUARIO INGRESA ES ENTERO O ES UN NUMERO IMPAR.
+# 2025 / 03/ 03 - V. 1. 0. 0 - SE AGREGAN COMENTARIOS Y ECANBEZADO AL CÓDIGO
+# TRABAJARON: NADIA / EDUARDO 
+# Solicita al usuario ingresar un número y lo convierte en entero 
+numero = int(input("Ingrese un número: "))  # La función 'int()' convierte la cadena de texto en un número entero.
+
+# Verifica si el número es divisible entre 2 sin dejar residuo
+if numero % 2 == 0: # Si el residuo de la división de 'numero' entre 2 es igual a 0, es un número par
+    # Si el número es divisible por 2, es par
+    print("El número es par") # La función print() muestra un mensaje en la pantalla en este caso "el numero es par 
 else:
-    print("El número es impar")
+    # Si el número no es divisible por 2, es impar
+    # Si la condición del 'if' no se cumple (es decir, el número no es divisible por 2), se ejecuta este bloque de código.
+    print("El número es impar")
