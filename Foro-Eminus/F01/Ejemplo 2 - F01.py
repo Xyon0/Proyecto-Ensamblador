cf209313521d1a9a50174b94c31dd5641025a965
McJosue1 <zS23013995@estudiantes.uv.mx>
2025-03-03 21:06:13 -0600

    Update Ejemplo 2 - F01.py

--- Foro-Eminus/F01/Ejemplo 2 - F01.py
+++ Foro-Eminus/F01/Ejemplo 2 - F01.py
@@ -1,6 +1,6 @@
 #Dansue
 #Este codigo fue realizado por Daniel Alberto y Josué, en el se muestra la lista que "deben" de aparecer de las frutas al momento de imprimirse agregando a la pera.
-
+#03/marzo/2025
 
 
 frutas = ["manzana", "plátano", "naranja"]
