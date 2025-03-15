2d93080a20a996b814df1a13d90d8df1b28a4fc0
IvonGlz <zs23013989@estudiantes.uv.mx>
2025-03-14 22:00:38 -0600

    agrego una carpeta
    se gregaron dos archivos que corresponden a una practica y esos códigos se encuentran dentro de la carpeta

---
+++ ProgramacionEnEnsambler/Practica 1/Ejercicio GIF.asm
@@ -0,0 +1,51 @@
+;este codigo logra mover un pixel a traves de la primera fila de la pantalla infinitamente como un gif, a diferencia de la primera version con ayuda de una instruccion es capas de repetirse infinitamente
+
+
+	
+
+JMP boot
+
+vslDisplay EQU 0x300 ;se le da a vslDisplay el valor de 0x300
+
+
+sprite: 								;se define el arreglo sprite con un valor hexadecimal que es un pixel de color 
+	DB "\xC4"
+
+Time:	DB "123456789101112131415161718192021222324252627282930"	; se define el arreglo Time con diferentes numeros para dar paso a un pequeño tiempo
+		DB 0							; se define el fin del arreglo que es 0 
+
+boot:
+	MOV C, sprite		; se le da a C el valor que sprite tenga 
+	MOV D, vslDisplay	; se le da a D el valor de vlsDisplay que es una posicion 
+   	MOV B, Time		; se le da al arreglo B el valor que Time tiene
+     
+
+.loop:
+	DEC D                   ; se disminuye en uno el valor de D para que no aparezca otra vez el pixel anterior 
+	MOVB [D], 0xFF		; la posicion que D representa en la pantalla se define como un pixel blanco 
+   
+    
+    
+    
+	INC D			; se aumenta en uno D que es una posicion 
+	MOVB AL, [C]		; se le da a AL el valor que C tenga 
+	MOVB [D], AL		; se le da a la posiciion en la memoria que representa D el valor que AL tenga 
+	INC D 			; se aumenta en uno el valor que D tenga 
+	CMP D, 0x30F		; se compara el valor de D con 0x30F  
+    
+	JNZ .loop               ; si son iguales si finaliza la subrutina
+    DEC D 			; se disminuye en uno el valor de D para que no aparezca otra vez el pixel anterior 
+    MOVB [D], 0xFF	        ; la posicion que D representa en la pantalla se define como un pixel blanco 
+    MOV C, sprite		; se le da a C el valor que sprite tenga 
+    MOV D, vslDisplay	        ; se le da a D el valor de vlsDisplay que es una posicion 
+		
+    JMP .loop                   ; se regresa a .loop infinitamente 
+    
+	;HLT			;el HLT se elimina para no afectar el bucle infinito 
+    
+    
+    
+    
+    
+    
+    
