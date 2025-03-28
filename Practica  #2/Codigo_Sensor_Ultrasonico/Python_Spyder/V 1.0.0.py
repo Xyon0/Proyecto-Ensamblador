10d79697d852fac183682a8f4aa4069c0b4e6cc9
Xyon0 <final_fantasi_xv@outlook.com>
2025-03-28 15:47:46 -0600

    Update V 1.0.0.py

--- Practica  #2/Codigo_Sensor_Ultrasonico/Python_Spyder/V 1.0.0.py
+++ Practica  #2/Codigo_Sensor_Ultrasonico/Python_Spyder/V 1.0.0.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 #Version 1.0.0
-import serial 
+import serial # Importa la biblioteca para la comunicación serial.
+
+# Configura el puerto serial para comunicarse con el Arduino.
+# "COM3" es el puerto donde está conectado el Arduino y 9600 es la velocidad de baudios.
 arduinoSerialData = serial.Serial("COM3", 9600) 
-while True:
-    if (arduinoSerialData.inWaiting ()> 0): 
-        myData = arduinoSerialData.readline() 
-        datos = float(myData) 
-        print(datos)
+
+while True: # Bucle infinito para leer datos continuamente
+    if (arduinoSerialData.inWaiting ()> 0):     # Verifica si hay datos disponibles para leer en el puerto serial.
+        myData = arduinoSerialData.readline()    #Lee los datos que obtiene del arudino.
+        datos = float(myData)    # Convierte los datos del arduino
+        print(datos)    # Imprime el valor de datos.
