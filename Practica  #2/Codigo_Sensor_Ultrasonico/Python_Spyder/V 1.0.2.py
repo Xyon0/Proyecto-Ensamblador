ac8c73ab765301bfdc8d3b7b2f2193a5a15c2b86
Xyon0 <final_fantasi_xv@outlook.com>
2025-03-28 15:43:08 -0600

    Update V 1.0.2.py
    Le puse comentarios a las lineas

--- Practica  #2/Codigo_Sensor_Ultrasonico/Python_Spyder/V 1.0.2.py
+++ Practica  #2/Codigo_Sensor_Ultrasonico/Python_Spyder/V 1.0.2.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
+# -*- coding: latin1 -*-
 #Version 1.0.2
-import serial
+import serial  # Importa la biblioteca para la comunicación serial
 
-arduinoSerialData = serial.Serial("COM3", 9600)
+# Configura el puerto serial para comunicarse con el Arduino
+# "COM3" es el puerto donde está conectado el Arduino y 9600 es la velocidad de baudios
+arduinoSerialData = serial.Serial("COM3", 9600) 
 
-while True:
-    if arduinoSerialData.inWaiting() > 0:
+while True:    # Bucle infinito para leer datos continuamente
+    if arduinoSerialData.inWaiting() > 0:    # Verifica si hay datos disponibles para leer en el puerto serial
         myData = arduinoSerialData.readline().decode('latin1').strip()  # Decodifica y elimina espacios en blanco
         try:
             distance = float(myData.split(':')[1])  # Divide la cadena y toma la parte después de ':'
-            print(distance)
-        except (ValueError, IndexError) as e:
-            print(f"Error al convertir a float: {e}")
+            print(distance)    # Imprime el valor de distancia
+        except (ValueError, IndexError) as e:    # Captura errores de conversión o de índice
+            print(f"Error al convertir a float: {e}")    # Imprime el error si ocurre un problema
