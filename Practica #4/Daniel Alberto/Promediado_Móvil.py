82318d7c24d7e28be7b08455c08ddac2f548d182
Xyon0 <final_fantasi_xv@outlook.com>
2025-05-09 22:09:20 -0600

    Update Promediado_Móvil.py

--- Practica #4/Daniel Alberto/Promediado_Móvil.py
+++ Practica #4/Daniel Alberto/Promediado_Móvil.py
@@ -1,4 +1,4 @@
-#Version 2.1.0
+#Version 3.0.0
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,24 +19,28 @@ plt.figure(figsize=(10, 8))
 
 plt.subplot(4,1,1)
 plt.plot(tiempo(temperatura),temperatura["Temperatura_C"] , color = "orange")
+plt.plot(tiempo(temperatura), temperatura["Promediado movil"], color = "blue", linestyle = "--")
 plt.title("Señal original - Temperatura")
-plt.ylabel("Temperatura - Centigrados")
+plt.ylabel("Temperatura - °C")
 plt.grid()
 
 plt.subplot(4,1,2)
 plt.plot(tiempo(humedad), humedad ["Humedad_Relativa_%"], color = "green")
+plt.plot(tiempo(temperatura), humedad["Promediado movil"], color = "red", linestyle = "--")
 plt.title("Señal original - Humedad")
 plt.ylabel("Humedad - Relativa")
 plt.grid()
 
 plt.subplot(4,1,3)
 plt.plot(tiempo(viento), viento["Velocidad_Viento_mps"])
+plt.plot(tiempo(viento), viento["Velocidad_Viento_mps"], color = "orange", linestyle = "--")
 plt.title("Señal original - Viento")
 plt.ylabel("Velacidad - Mps")
 plt.grid()
 
 plt.subplot(4,1,4)
 plt.plot(tiempo(viento), viento["Direccion_Viento_deg"], color = "grey")
+plt.plot(tiempo(viento), viento["Direccion_Viento_deg"], color = "purple", linestyle = "--")
 plt.title("Señal original - Viento")
 plt.ylabel("Dirección - DEG")
 plt.grid()
